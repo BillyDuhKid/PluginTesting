d33440a5e05f1364d7630ce694f4982fc8bb02e2
Billy Chatterton <44380281+BillyDuhKid@users.noreply.github.com>
2024-05-06 23:07:17 -0700

    Update Project to support Common UI - Useful changes to track here!
    Take not of .ini file changes for future plugin installs.

--- !cleanupAll.bat
+++ !cleanupAll.bat
@@ -2,7 +2,7 @@ rmdir /s /q .vs
 rmdir /s /q Binaries
 rmdir /s /q Intermediate
 rmdir /s /q Saved
-rmdir /s /q Plugins\TAB_Game\Binaries
-rmdir /s /q Plugins\TAB_Game\Intermediate
 del /s .vsconfig
 del /s *.sln
+cd Plugins\TAB_Game\
+!cleanup.bat
